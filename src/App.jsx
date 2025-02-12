b01668e9b29417670241f83642ec86bd34b9dda8
EsteNoEsMonti <estenoesmonti@gmail.com>
2025-02-12 12:28:09 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Body from "./components/Body"
+import NavBar from "./components/NavBar"
+
+function App() {
+
+  return (
+    <>
+      <NavBar />
+      <Body />
+    </>
+  )
+}
+
+export default App
